c504e87432d584a7b6fe9011cdc96bf4522c3ef9
Akshit Solanki <solankiakii28@gmail.com>
2024-04-18 22:38:46 +0530

    update

--- src/components/PostCard.jsx
+++ src/components/PostCard.jsx
@@ -9,7 +9,7 @@ function PostCard({ $id, title, featuredImage }) {
       <div className='w-full bg-[#0C0C0E] flex flex-col items-center justify-center rounded-xl p-4 transition-transform duration-300 ease-in-out transform hover:scale-105 hover:shadow-lg '>
         <div className='w-full flex items-center justify-center mb-4'>
           <img src={appwriteService.getFilePreview(featuredImage)} alt={title}
-            className=' rounded-xl  object-contain w-full h-[280px] md:h-[220px] ' />
+            className='object-contain w-full h-[280px] md:h-[220px] ' />
         </div>
         <h2
           className='text-xl font-bold max-w-[280px] overflow-hidden whitespace-nowrap overflow-ellipsis'
